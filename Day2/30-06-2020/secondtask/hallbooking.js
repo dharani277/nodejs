293ad4a00c87012ef5c2f412d7b8ff85cd6ba2e4
dharani277 <60962689+dharani277@users.noreply.github.com>
2020-06-30 17:20:25 +0530

    Create hallbooking.js

---
+++ Day2/30-06-2020/secondtask/hallbooking.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.post("/createRoom", (req, res) => {
+  createroom.push(req.body);
+  res.json({ request: "received" });
+});
+app.post("/room", (req, res) => {
+  room.push(req.body);
+  res.json({ request: " post received" });
+});
+app.get("/bookedData", (req, res) => {
+  let details = room.map((data) => {
+    return {
+      Room_ID: data.Room_ID,
+      booked_Stated: "Booked",
+      customer_name: data.Customer_Name,
+      Date: data.Date,
+      Start_Time: data.Start_Time,
+      End_Time: data.End_Time,
+    };
+  });
+  res.json(details);
+});
+app.get("/customersData", (req, res) => {
+  let details = room.map((data) => {
+    return { customer_name: data.Customer_Name, Room_ID: data.Room_ID };
+  });
+
+  res.json(details);
+});
+app.listen(process.env.PORT || port, () => {
+  console.log("welcome to Meeting Hall Booking");
+});
